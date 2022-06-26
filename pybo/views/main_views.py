aca437671d59e1b757769f192e174ec13f01d482
unknown <913breeze@gmail.com>
2022-06-26 15:51:32 +0900

    feat : 기능 목적에 따른 소스 분할
    - views 내 main_views 에 있던 페이지 blueprint를 이용 분리

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,6 +1,5 @@
-from flask import Blueprint, render_template
-
-from pybo.models import Question
+from flask import Blueprint, url_for
+from werkzeug.utils import redirect
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
@@ -8,16 +7,6 @@ bp = Blueprint('main', __name__, url_prefix='/')
 def hello_pybo():
     return 'Hello, Pybo!'
 
-@bp.route('/detail/<int:question_id>')
-def detail(question_id):
-    question = Question.query.get_or_404(question_id)
-    return render_template('question/question_detail.html',
-                           question=question)
-    #return 'Hello, Pybo!'
-
 @bp.route('/')
 def index():
-    question_list = Question.query.order_by(Question.create_date.desc())
-    return render_template('question/question_list.html',
-                           question_list=question_list)
-    # return 'Pybo index'
+    return redirect(url_for('question._list'))
