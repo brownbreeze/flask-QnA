c25a6546bb5b388999b8350739b29a86de84cd75
unknown <913breeze@gmail.com>
2022-06-26 15:18:49 +0900

    feat : db ORM 적용
    - models.py question, answer 클래스 생성
    - flask db init, migrate, upgrade 수행 완료
    - shell 을 이용해 db에 디비 자료 작성 완료

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,6 +1,25 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
-@app.route('/')
-def hello_pybo():
-    return 'Hello, Pybo!'
+import config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(config)
+
+    # ORM
+    db.init_app(app)
+    migrate.init_app(app, db)
+    from . import models
+
+    from .views import main_views
+    app.register_blueprint(main_views.bp)
+
+    return app
+
