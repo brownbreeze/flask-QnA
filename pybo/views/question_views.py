01d351fafd59ef8c707d09ef3c18f51044d0c195
unknown <913breeze@gmail.com>
2022-07-03 22:29:01 +0900

    feat : Answer 모델에 글쓴이 추가 기능
    - 질문, 답변 등록에 글쓴이 정보 연결

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, url_for
+from flask import Blueprint, render_template, request, url_for, g
 from werkzeug.utils import redirect
 from datetime import datetime
 
@@ -28,7 +28,7 @@ def create():
     form = QuestionForm()
     if request.method == 'POST' and form.validate_on_submit() :
         question = Question(subject=form.subject.data, content=form.content.data,
-                            create_date=datetime.now())
+                            create_date=datetime.now(), user=g.user)
         db.session.add(question)
         db.session.commit()
         return redirect(url_for('main.index'))
