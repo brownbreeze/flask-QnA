60888b15c136fd01fd7f613037a13e10f46711a0
unknown <913breeze@gmail.com>
2022-07-06 00:29:14 +0900

    feat : 검색 기능 추가
    - 아우터 조인을 이용한 검색 기능

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, url_for, g, flash
 from werkzeug.utils import redirect
 from datetime import datetime
 
-from pybo.models import Question
+from pybo.models import Question, Answer, User
 from .. import db
 from ..forms import AnswerForm, QuestionForm
 from pybo.views.auth_views import login_required
@@ -65,3 +65,31 @@ def delete(question_id):
     db.session.delete(question)
     db.session.commit()
     return redirect(url_for('question._list'))
+
+@bp.route('/list/')
+@login_required
+def _list():
+    page = request.args.get('page', type=int, default=1)
+    kw=request.args.get('kw', type=str, default='')
+
+    question_list = Question.query.order_by(Question.create_date.desc())
+    if kw:
+        search = '%%{}%%'.format(kw)
+        sub_query = db.session.query(
+            Answer.question_id, Answer.content, User.username).join(
+            User, Answer.user_id == User.id).subquery()
+        question_list = question_list \
+            .join(User) \
+            .outerjoin(sub_query, sub_query.c.question_id = Question.id)\
+            .filter(Question.subject.ilike(search)|
+                    Question.content.ilike(search)|
+                    User.username.ilike(search)|
+                    sub_query.c.content.ilike(search)|
+                    sub_query.c.username.ilike(search)
+                    )\
+        .distinct()
+
+    # 페이징
+    question_list = question_list.paginate(page, per_page=10)
+    return render_template('question/question_list.html',
+                           question_list=question_list, page=page, kw=kw)
