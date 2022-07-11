4c7188a7139823fe28cab8c12d49232077ad45ab
unknown <913breeze@gmail.com>
2022-07-11 23:13:29 +0900

    feat : config 변경
    - development 설정

--- config/production.py
+++ config/production.py
@@ -1,7 +1,5 @@
-import os
-
-
-BASE_DIR = os.path.dirname(__file__)
+from config.default import *
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = b'G\xe2(\x97\xa1\x13K\x00\xc5\xad\x14\x8e\xfb4\xfb\x10'
--- pybo/__init__.py
+++ pybo/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flaskext.markdown import Markdown
 
-import config
+# import config
 
 naming_convention = {
     "ix":"ix_%(column_0_label)s",
@@ -18,7 +18,9 @@ migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(config)
+    #app.config.from_object(config)
+    app.config.from_envvar('APP_CONFIG_FILE')
+
     # 임시방편 self.csrf_impl.generate_csrf_token(self) 관련 에러
     app.config["SECRET_KEY"]="1273128736178"
 
