32e00f69a3e6460a8ca791be377d6987fb8a3eae
unknown <913breeze@gmail.com>
2022-07-14 21:04:49 +0900

    fix : 로깅 버그 테스트
    - 로깅 버그 테스트를 위한 production.py 수정

--- config/production.py
+++ config/production.py
@@ -7,24 +7,24 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'G\xe2(\x97\xa1\x13K\x00\xc5\xad\x14\x8e\xfb4\xfb\x10'
 
 dictConfig({
-    'version':1,
-    'formatters':{
-        'default':{
-            'format':'[%(asctime)s%] %(levelname)s in %(module)s: %(message)s',
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
         }
     },
-    'handlers':{
-        'file':{
-            'level':'INFO',
-            'class':'logging.handlers.RotatingFileHandler',
-            'filename':os.path.join(BASE_DIR, 'logs/myproject.log'),
-            'maxBytes':1024*1024*5, # 5 MB
-            'backupCount':5,
-            'formatter':'default',
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 5,
+            'formatter': 'default',
         },
     },
-    'root':{
-        'level':'INFO',
-        'handlers':['file']
+    'root': {
+        'level': 'INFO',
+        'handlers': ['file']
     }
 })
